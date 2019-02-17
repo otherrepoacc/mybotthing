ff7423ee2842bacbf16d32606a79225539a800f8
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-16 19:14:28 -0700

    Update kick.js

--- commands/tools/kick.js
+++ commands/tools/kick.js
@@ -21,7 +21,7 @@ constructor(client)
         {
             if(args = message.mentions.users.first())
             {
-                message.mentions.users.first().kick('Member kicked by ' + message.member.name + '.')
+                message.mentions.member.first().kick('Member kicked by ' + message.member.name + '.')
             }
         }
     }
