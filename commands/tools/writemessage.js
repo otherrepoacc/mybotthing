72510510218c72500858b053addd905a7162188b
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-20 01:34:45 -0700

    Create writemessage.js

---
+++ commands/tools/writemessage.js
@@ -0,0 +1,29 @@
+const commando = require('discord.js-commando')
+const fs = require('fs')
+const messagesfile = require(__dirname + '/messages.json')
+class WriteMessageCommand extends commando.Command
+{
+constructor(client)
+    {
+        super(client,{
+            name: 'write',
+            group: 'tools',
+            memberName: 'write',
+            description: 'Write message to json file'
+        });
+    }
+    async run(message, args)
+    {
+        messagesfile[message.member.displayName] =
+        {
+            message: message.content
+        }
+        
+        fs.writeFile(__dirname + '/messages.json, JSON.stringify(messagesfile, null, 4))
+        .then(function{
+           message.channel.send('Message written to messages.json')
+        })
+    }
+
+}
+    module.exports = WriteMesssageCommand
