812a02a7a0ad7ef24d9b6e76ba77131a3d28428b
otherrepoacc <47887169+otherrepoacc@users.noreply.github.com>
2019-02-22 03:37:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ bot.registry.registerCommandsIn(__dirname + '/commands');
 global.servers = {};
 global.green = '00FF00'
 
-bot.on('message', function(message){
-if(message.content == 'hi')
-{
-    message.channel.send('Hello, ' + message.author + '!');
-}
-});
+
+.
 
 
 
@@ -27,38 +23,15 @@ if(message.content == 'hi')
 bot.on('ready', function(){
     console.log('Bot is ready.');
 
-bot.on('guildMemberAdd', function(member){
-    if(!member.guild.channels.find('name', 'welcome'))
-    {
-        member.guild.createChannel('welcome', 'text')
-        .then(function()
-        {
-            let channel = member.guild.channels.find('name', 'welcome')
-            channel.send(member.displayName + ' has joined the server!')
-        })
-    }
-})
+
 
 });
 bot.on('guildMemberAdd', function(member){
-    if(!member.guild.roles.find("name", 'Member'))
-    {
-        member.guild.createRole({
-            name: 'Member',
-            color: '00FF00'
-        }).then(function()
-        {
+    
+    
         member.send('Welcome to ' + member.guild.name + '!')
-        let memberRole = member.guild.roles.find("name", 'Member');
-        member.addRole(memberRole);
-        })
-    }
-    else
-    {
-        member.send('Welcome to ' + member.guild.name + '!')
-        let memberRole = member.guild.roles.find("name", 'Member');
-        member.addRole(memberRole);
-    }
+        
+    
 });
 bot.on('guildCreate', function(){   /* this code changes the bot activity to "Listening to (amount of servers bot is in) | @botname help" when the bot is added to a server or removed from a server (this is way too long) */
     bot.user.setActivity('' + bot.guilds.size + ' servers @Automatic help', {type: 'LISTENING'})
