681640aee30ad45d5fbbba2c4b2eb0e4d8b544de
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-21 20:08:03 -0700

    Create post.js

---
+++ post.js
@@ -0,0 +1,6 @@
+const url='https://discordbots.org/api';
+Http.open("POST", url);
+Http.send('Authorization:	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI2NDgxMTYxMzcwODc0Njc1MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNDgzMDk5MjAwfQ.8tpNASxdSsfkVF7YparhyV1Ouy5ORQ3AM2jitd_Y-PI');
+Http.onreadystatechange=(e)=>{
+console.log(Http.responseText)
+}
